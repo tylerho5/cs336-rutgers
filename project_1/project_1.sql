cd510c56740d2cf2647f6f4d8994885b3e345da4
Tyler Ho <tylerho6752@gmail.com>
2025-04-06 19:48:45 -0400

    Finished insertion of location data

--- project_1/project_1.sql
+++ project_1/project_1.sql
@@ -408,76 +408,191 @@ WHERE as_of_year != '' AND respondent_id != '' AND agency_code != '';
 
 -- fill ApplicantRace table
 INSERT INTO ApplicantRace (ID, race_number, race_code)
-SELECT ID, 1, NULLIF(applicant_race_1, '')::SMALLINT
+SELECT ID, 1, NULLIF(applicant_race_1, '')::TINYINT
 FROM Preliminary
 WHERE applicant_race_1 != '';
 
 INSERT INTO ApplicantRace (ID, race_number, race_code)
-SELECT ID, 2, NULLIF(applicant_race_2, '')::SMALLINT
+SELECT ID, 2, NULLIF(applicant_race_2, '')::TINYINT
 FROM Preliminary
 WHERE applicant_race_2 != '';
 
 INSERT INTO ApplicantRace (ID, race_number, race_code)
-SELECT ID, 3, NULLIF(applicant_race_3, '')::SMALLINT
+SELECT ID, 3, NULLIF(applicant_race_3, '')::TINYINT
 FROM Preliminary
 WHERE applicant_race_3 != '';
 
 INSERT INTO ApplicantRace (ID, race_number, race_code)
-SELECT ID, 4, NULLIF(applicant_race_4, '')::SMALLINT
+SELECT ID, 4, NULLIF(applicant_race_4, '')::TINYINT
 FROM Preliminary
 WHERE applicant_race_4 != '';
 
 INSERT INTO ApplicantRace (ID, race_number, race_code)
-SELECT ID, 5, NULLIF(applicant_race_5, '')::SMALLINT
+SELECT ID, 5, NULLIF(applicant_race_5, '')::TINYINT
 FROM Preliminary
 WHERE applicant_race_5 != '';
 
 -- fill CoApplicantRace table
 INSERT INTO CoApplicantRace (ID, race_number, race_code)
-SELECT ID, 1, NULLIF(co_applicant_race_1, '')::SMALLINT
+SELECT ID, 1, NULLIF(co_applicant_race_1, '')::TINYINT
 FROM Preliminary
 WHERE co_applicant_race_1 != '';
 
 INSERT INTO CoApplicantRace (ID, race_number, race_code)
-SELECT ID, 2, NULLIF(co_applicant_race_2, '')::SMALLINT
+SELECT ID, 2, NULLIF(co_applicant_race_2, '')::TINYINT
 FROM Preliminary
 WHERE co_applicant_race_2 != '';
 
 INSERT INTO CoApplicantRace (ID, race_number, race_code)
-SELECT ID, 3, NULLIF(co_applicant_race_3, '')::SMALLINT
+SELECT ID, 3, NULLIF(co_applicant_race_3, '')::TINYINT
 FROM Preliminary
 WHERE co_applicant_race_3 != '';
 
 INSERT INTO CoApplicantRace (ID, race_number, race_code)
-SELECT ID, 4, NULLIF(co_applicant_race_4, '')::SMALLINT
+SELECT ID, 4, NULLIF(co_applicant_race_4, '')::TINYINT
 FROM Preliminary
 WHERE co_applicant_race_4 != '';
 
 INSERT INTO CoApplicantRace (ID, race_number, race_code)
-SELECT ID, 5, NULLIF(co_applicant_race_5, '')::SMALLINT
+SELECT ID, 5, NULLIF(co_applicant_race_5, '')::TINYINT
 FROM Preliminary
 WHERE co_applicant_race_5 != '';
 
 -- fill DenialReasons table
 INSERT INTO DenialReasons (ID, reason_number, denial_reason_code)
-SELECT ID, 1, NULLIF(denial_reason_1, '')::SMALLINT
+SELECT ID, 1, NULLIF(denial_reason_1, '')::TINYINT
 FROM Preliminary
 WHERE denial_reason_1 != '';
 
 INSERT INTO DenialReasons (ID, reason_number, denial_reason_code)
-SELECT ID, 2, NULLIF(denial_reason_2, '')::SMALLINT
+SELECT ID, 2, NULLIF(denial_reason_2, '')::TINYINT
 FROM Preliminary
 WHERE denial_reason_2 != '';
 
 INSERT INTO DenialReasons (ID, reason_number, denial_reason_code)
-SELECT ID, 3, NULLIF(denial_reason_3, '')::SMALLINT
+SELECT ID, 3, NULLIF(denial_reason_3, '')::TINYINT
 FROM Preliminary
 WHERE denial_reason_3 != '';
 
 -- create temp table to handle location data
+CREATE TEMPORARY TABLE temp_location AS
+SELECT DISTINCT
+    NULLIF(msamd, '') AS msamd,
+    NULLIF(state_code, '') AS state_code,
+    NULLIF(county_code, '') AS county_code,
+    NULLIF(census_tract_number, '') AS census_tract_number,
+    NULLIF(population, '')::INTEGER AS population,
+    NULLIF(minority_population, '')::NUMERIC AS minority_population,
+    NULLIF(hud_median_family_income, '')::INTEGER AS hud_median_family_income,
+    NULLIF(tract_to_msamd_income, '')::NUMERIC AS tract_to_msamd_income,
+    NULLIF(number_of_owner_occupied_units, '')::INTEGER AS number_of_owner_occupied_units,
+    NULLIF(number_of_1_to_4_family_units, '')::INTEGER AS number_of_1_to_4_family_units,
+    CONCAT(
+        COALESCE(msamd, ''), '|',
+        COALESCE(state_code, ''), '|',
+        COALESCE(county_code, ''), '|',
+        COALESCE(census_tract_number, ''), '|',
+        COALESCE(population, ''), '|',
+        COALESCE(minority_population, ''), '|',
+        COALESCE(hud_median_family_income, ''), '|',
+        COALESCE(tract_to_msamd_income, ''), '|',
+        COALESCE(number_of_owner_occupied_units, ''), '|',
+        COALESCE(number_of_1_to_4_family_units, '')
+    ) as location_key
+FROM Preliminary;
 
--- fill location table with distinct values from temp table
+-- fill location table with values from temp table
+INSERT INTO Location (
+    msamd,
+    state_code,
+    county_code,
+    census_tract_number,
+    population,
+    minority_population,
+    hud_median_family_income,
+    tract_to_msamd_income,
+    number_of_owner_occupied_units,
+    number_of_1_to_4_family_units
+)
+SELECT
+    msamd,
+    state_code,
+    county_code,
+    census_tract_number,
+    population,
+    minority_population,
+    hud_median_family_income,
+    tract_to_msamd_income,
+    number_of_owner_occupied_units,
+    number_of_1_to_4_family_units
+FROM temp_location;
 
 -- create temp table for creating location_id
+CREATE TEMPORARY TABLE temp_location_id AS
+SELECT 
+    temp.location_id
+    loc.location_id,
+from temp_location temp
+JOIN Location loc ON 
+    (temp.msamd = loc.msamd OR (temp.msamd IS NULL AND loc.msamd IS NULL)) AND
+    (temp.state_code = loc.state_code OR (temp.state_code IS NULL AND loc.state_code IS NULL)) AND
+    (temp.county_code = loc.county_code OR (temp.county_code IS NULL AND loc.county_code IS NULL)) AND
+    (temp.census_tract_number = loc.census_tract_number OR (temp.census_tract_number IS NULL AND loc.census_tract_number IS NULL)) AND
+    (temp.population = loc.population OR (temp.population IS NULL AND loc.population IS NULL)) AND
+    (temp.minority_population = loc.minority_population OR (temp.minority_population IS NULL AND loc.minority_population IS NULL)) AND
+    (temp.hud_median_family_income = loc.hud_median_family_income OR (temp.hud_median_family_income IS NULL AND loc.hud_median_family_income IS NULL)) AND
+    (temp.tract_to_msamd_income = loc.tract_to_msamd_income OR (temp.tract_to_msamd_income IS NULL AND loc.tract_to_msamd_income IS NULL)) AND
+    (temp.number_of_owner_occupied_units = loc.number_of_owner_occupied_units OR (temp.number_of_owner_occupied_units IS NULL AND loc.number_of_owner_occupied_units IS NULL)) AND
+    (temp.number_of_1_to_4_family_units = loc.number_of_1_to_4_family_units OR (temp.number_of_1_to_4_family_units IS NULL AND loc.number_of_1_to_4_family_units IS NULL));
 
 -- use temp table to modify LoanApplication table to include location_id
+INSERT INTO LoanApplication (
+    ID, as_of_year, respondent_id, loan_type, property_type, loan_purpose,
+    owner_occupancy, loan_amount_000s, preapproval, action_taken,
+    location_id, applicant_ethnicity, co_applicant_ethnicity,
+    applicant_sex, co_applicant_sex, applicant_income_000s,
+    purchaser_type, rate_spread, hoepa_status, lien_status,
+    edit_status, sequence_number, application_date_indicator
+)
+SELECT
+    p.ID,
+    NULLIF(p.as_of_year, '')::INTEGER,
+    NULLIF(p.respondent_id, ''),
+    NULLIF(p.loan_type, '')::TINYINT,
+    NULLIF(p.property_type, '')::TINYINT,
+    NULLIF(p.loan_purpose, '')::TINYINT,
+    NULLIF(p.owner_occupancy, '')::TINYINT,
+    NULLIF(p.loan_amount_000s, '')::NUMERIC,
+    NULLIF(p.preapproval, '')::TINYINT,
+    NULLIF(p.action_taken, '')::TINYINT,
+    loc.location_id,
+    NULLIF(p.applicant_ethnicity, '')::TINYINT,
+    NULLIF(p.co_applicant_ethnicity, '')::TINYINT,
+    NULLIF(p.applicant_sex, '')::TINYINT,
+    NULLIF(p.co_applicant_sex, '')::TINYINT,
+    NULLIF(p.applicant_income_000s, '')::NUMERIC,
+    NULLIF(p.purchaser_type, '')::TINYINT,
+    NULLIF(p.rate_spread, ''),
+    NULLIF(p.hoepa_status, '')::TINYINT,
+    NULLIF(p.lien_status, '')::TINYINT,
+    NULLIF(p.edit_status, '')::TINYINT,
+    NULLIF(p.sequence_number, ''),
+    NULLIF(p.application_date_indicator, '')::TINYINT
+FROM Preliminary p
+JOIN temp_location_id loc ON
+    CONCAT(
+        COALESCE(p.msamd, ''), '|',
+        COALESCE(p.state_code, ''), '|',
+        COALESCE(p.county_code, ''), '|',
+        COALESCE(p.census_tract_number, ''), '|',
+        COALESCE(p.population, ''), '|',
+        COALESCE(p.minority_population, ''), '|',
+        COALESCE(p.hud_median_family_income, ''), '|',
+        COALESCE(p.tract_to_msamd_income, ''), '|',
+        COALESCE(p.number_of_owner_occupied_units, ''), '|',
+        COALESCE(p.number_of_1_to_4_family_units, '')
+    ) = loc.location_key;
+
+-- remove temp tables
+DROP TABLE temp_location;
+DROP TABLE temp_location_id;
