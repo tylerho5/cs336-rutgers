d78e582910498da896ddc31e8bdb3f384bed7e9d
Tyler Ho <tylerho6752@gmail.com>
2025-04-27 00:09:27 -0400

    Add ask loop, query extraction, and logging

--- project_2/database_llm.py
+++ project_2/database_llm.py
@@ -1,27 +1,51 @@
-import llm_manager
-import ssh_handler
+import time
+
 import ilab_script
+import llm_manager
+import query_extraction
+import ssh_handler
+
 
 model_name = "Phi-3.5-mini-instruct-Q4_K_M.gguf"
 
-
 def main():
-    # Load the database schema
+    # load the database schema
     context = llm_manager.load_schema()
 
-    # Initialize the LLM
+    # initialize the llm
     llm = llm_manager.initalize_llm(model_name)
 
-    # Get the question from the user
-    question = input("Enter your question: ")
+    while True:
 
-    # Build the prompt for the LLM
-    prompt = llm_manager.build_prompt(context, question)
+        timestamp = time.strftime('%I:%M:%S %p %m/%d/%y', time.localtime(time.time()))
 
-    # Query the LLM
-    sql_query = llm_manager.query_llm(llm, prompt)
+        # get the question from the user
+        question = input("Enter your question: ")
+        question = question.strip()
 
-    print(f"Generated SQL Query: {sql_query}")
+        if question.lower() == "exit":
+            print("Exiting...")
+            break
+
+        # build the prompt for the llm
+        prompt = llm_manager.build_prompt(context, question)
+
+        # prompt the llm
+        response = llm_manager.query_llm(llm, prompt)
+        response = response.strip()
+
+        with open('llm_output.txt', 'a') as f:
+            f.write("\n\n==================== LOG ENTRY START ====================\n")
+            f.write(f"Timestamp: {timestamp}\n")
+            f.write(f"Question: {question}\n\n")
+            f.write("--- LLM Response Start ---\n")
+            f.write(f"{response}\n")
+            f.write("--- LLM Response End ---\n\n")
+            f.write("==================== LOG ENTRY END ======================\n\n")
+
+        query = query_extraction.extract_query_from_text(response)
+
+        print(f"Generated response: \n{query}\n")
 
 if __name__ == "__main__":
     main()
