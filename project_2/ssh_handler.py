a9096b149d47d3938c44d9a42d25bf80a93eb77d
Tyler Ho <tylerho6752@gmail.com>
2025-04-30 03:09:06 -0400

    Implement paramiko to connect to iLab

--- project_2/ssh_handler.py
+++ project_2/ssh_handler.py
@@ -2,7 +2,50 @@ import getpass
 import paramiko
 
 def get_ssh_credentials():
-    pass 
+    '''
+    gets user, host, and pwd (securely)
+    '''
 
-def execute_query():
-    pass
+    host = input("Enter iLab hostname: ")
+
+    user = input("Enter NetID: ")
+
+    pwd = getpass.getpass(f"Enter pwd for {user}@{host}: ")
+
+    return host, user, pwd
+
+def execute_query(host, user, pwd, query, ilab_script_path):
+    '''
+    connects to iLab and runs iLab script, passing query
+    '''
+
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.RejectPolicy())
+
+    try:
+        client.connect(host, username=user, password=pwd, timeout=10)
+
+        command = f"python3 {ilab_script_path} '{query}"
+
+        stdin, stdout, stderr = client.exec_command(command, timeout=60)
+        output = stdout.read().decode('utf-8').strip()
+        error = stderr.read().decode('utf-8').strip()
+
+        if error:
+            print(f"Command error:\n{error}")
+
+        return output
+    
+    except paramiko.AuthenticationException:
+        print("Authentication failed. Check user and password.")
+        return None
+    except paramiko.SSHException as sshException:
+        print(f"Unable to establish connection: {sshException}")
+        return None
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return None
+    
+    finally:
+        if client:
+            client.close()
