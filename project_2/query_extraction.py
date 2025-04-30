f3cd8b3adf652d0b18c3730a52b96b29faaa893b
Tyler Ho <tylerho6752@gmail.com>
2025-04-30 03:08:32 -0400

    Update regex expression

--- project_2/query_extraction.py
+++ project_2/query_extraction.py
@@ -5,8 +5,10 @@ def extract_query_from_text(text):
     extract SQL query from llm output
     """
 
-    pattern = r"```sql\s*([\s\S]*?)\s*```"
+    pattern = r"\`\`\`sql\s*([\s\S]*?)\s*\`\`\`"
 
     match = re.search(pattern, text)
 
     return match[1]
+
+
