9e9b996c566df74fb6010d2929f8e0a285cbb7fd
Tyler Ho <tylerho6752@gmail.com>
2025-05-02 19:20:00 -0400

    Create runner_script.py

---
+++ project_2/runner_script.py
@@ -0,0 +1,27 @@
+import os
+import platform
+import subprocess
+import sys
+ 
+# set script path relative to the root directory
+script_path = os.path.join("project_2", "database_llm.py")
+
+# check if script exists
+if not os.path.exists(script_path):
+    print(f"Error: Script not found at '{script_path}'.")
+    quit()
+
+# prep environment variables
+env = os.environ.copy()
+env["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+# prep command
+command = [sys.executable, script_path]
+
+current_platform = platform.system()
+
+try:
+    process = subprocess.run(command, env=env, check=True, text=True)
+
+except Exception as e:
+    print(f"Error occurred: {e}")
